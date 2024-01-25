434cc0b9fe84e8819384676eb23c55e82a99b989
Gabriell <gabrielll9652@gmail.com>
2024-01-25 14:35:23 -0300

    projeto inicial

---
+++ teste/test.js
@@ -0,0 +1,16 @@
+
+
+function dobro(){
+    let num = document.getElementById('numeroDobro').value;
+    resultado = parseInt(num * 2);
+    
+    return resultado;
+
+}
+console.log (dobro());
+
+
+
+
+
+
